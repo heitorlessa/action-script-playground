c3975d4bf6d8d21a4ae6d06c4b779c4839bbefff
heitorlessa <lessa@amazon.co.uk>
2022-07-11 14:02:06 +0200

    chore(ci): exp graphql

--- .github/scripts/label_related_issue.js
+++ .github/scripts/label_related_issue.js
@@ -5,35 +5,22 @@ module.exports = async ({github, context, core}) => {
     console.dir(pr,{depth:null})
     core.info("Payload as it comes..")
     core.info(JSON.stringify(pr));
-    return "Hello"
 
-    // core.info(`Is it a PR Merge: ${pr_event.pull_request.merged}`)
-    const prBody = pr_event.body;
-    const prNumber = pr_number;
-    const releaseLabel = process.env.RELEASE_LABEL;
-    const maintainersTeam = process.env.MAINTAINERS_TEAM
+    const query = `query linkedIssues($url:URI!) {
+        resource(url:$url) {
+          ... on PullRequest {
+            closingIssuesReferences(first: 10) {
+              nodes {
+                number
+              }
+            }
+          }
+        }
+    }`;
+    
+    let variables = {url: "https://github.com/heitorlessa/action-script-playground/pull/20"}
+    const result = await github.graphql(query, variables)
+    console.dir(result,{depth:null})
 
-
-    // const RELATED_ISSUE_REGEX = /Issue number:[^\d\r\n]+(?<issue>\d+)/;
-
-    // const isMatch = RELATED_ISSUE_REGEX.exec(prBody);
-    // if (!isMatch) {
-    //   core.setFailed(`Unable to find related issue for PR number ${prNumber}.\n\n Body details: ${prBody}`);
-    //   return await github.rest.issues.createComment({
-    //     owner: context.repo.owner,
-    //     repo: context.repo.repo,
-    //     body: `${maintainersTeam} No related issues found. Please ensure '${releaseLabel}' label is applied before releasing.`,
-    //     issue_number: prNumber,
-    //   });
-    // }
-
-    // const { groups: {relatedIssueNumber} } = isMatch
-
-    // core.info(`Auto-labeling related issue ${relatedIssueNumber} for release`)
-    // return await github.rest.issues.addLabels({
-    //   issue_number: relatedIssueNumber,
-    //   owner: context.repo.owner,
-    //   repo: context.repo.repo,
-    //   labels: [releaseLabel]
-    // })
+    return pr
 }
