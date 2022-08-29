9faa1a24361818060c3ddf3c52b3dd816899fc4b
heitorlessa <lessa@amazon.co.uk>
2022-08-29 17:21:00 +0200

    chore(ci): destructure assignment

--- .github/scripts/comment_on_large_pr.js
+++ .github/scripts/comment_on_large_pr.js
@@ -1,11 +1,11 @@
 module.exports = async ({github, context, core}) => {
     /** @type {string[]} */
-    const labels = await github.rest.issues.listLabelsOnIssue({
+    const { data: labels } = await github.rest.issues.listLabelsOnIssue({
       owner: context.repo.owner,
       repo: context.repo.repo,
       issue_number: process.env.PR_NUMBER,
     })
 
-    console.log(labels.data)
+    console.log(labels)
     return labels
 }
